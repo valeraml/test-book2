6ba7e677c1726a5a530b2e4d91f962e5bfd62f8a
valeraml <valeraml@gmail.com>
2024-04-11 16:17:55 +0000

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -25,7 +25,7 @@ var ptx_lunr_docs = [
   "type": "Chapter",
   "number": "2",
   "title": "Chapter 1",
-  "body": " Chapter 1   Text before the first section.  Some section  Some section pic1.jpg   A pendulum   a third degree polynomial with a local max and a local min; its derivative is plotted on the same axes    another   A pendulum simple.jpg   a third degree polynomial with a local max and a local min; its derivative is plotted on the same axes     A pendulum ball.svg   a third degree polynomial with a local max and a local min; its derivative is plotted on the same axes     A pendulum matter.svg   a third degree polynomial with a local max and a local min; its derivative is plotted on the same axes    sgv          A simple eigenvector demonstration     "
+  "body": " Chapter 1   Text before the first section.  Some section  Some section pic1.jpg   A pendulum   a third degree polynomial with a local max and a local min; its derivative is plotted on the same axes    another   A pendulum simple.jpg   a third degree polynomial with a local max and a local min; its derivative is plotted on the same axes     A pendulum ball.svg   a third degree polynomial with a local max and a local min; its derivative is plotted on the same axes     A pendulum matter.svg   a third degree polynomial with a local max and a local min; its derivative is plotted on the same axes    sgv          A simple eigenvector demonstration     1-Dimensional Kinematics     "
 },
 {
   "id": "figure-function-derivative",
@@ -80,6 +80,15 @@ var ptx_lunr_docs = [
   "number": "2.6",
   "title": "",
   "body": " A simple eigenvector demonstration   "
+},
+{
+  "id": "sec--10",
+  "level": "2",
+  "url": "ch-chapter-title.html#sec--10",
+  "type": "Figure",
+  "number": "2.7",
+  "title": "",
+  "body": " 1-Dimensional Kinematics   "
 },
 {
   "id": "backmatter-2",
